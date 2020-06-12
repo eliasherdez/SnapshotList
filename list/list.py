e1a0dee76f9d1e4c1cf45aab2d3bda15aef3d57f
Elias Hernandez <eliashernandez@macbook-pro.home>
2020-06-12 14:59:57 -0500

    Includes Snapshot Commands

--- list/list.py
+++ list/list.py
@@ -4,7 +4,7 @@ import click
 session = boto3.Session(profile_name='SnapshotList')
 ec2 = session.resource('ec2')
 
-def filter_instanes(project):
+def filter_instances(project):
     instances = []
 
     if project:
@@ -15,17 +15,95 @@ def filter_instanes(project):
 
     return instances
 
-
 @click.group()
+def cli():
+    """SnapshotList Manages Snapshots"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only snapshotd for project (tag Project:<name>)")
+
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                s.id,
+                v.id,
+                i.id,
+                s.state,
+                s.progress,
+                s.start_time.strftime("%c")
+            )))
+    return
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) +"GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+@cli.group('instances')
 def instances():
         """Commands for instances"""
+
+@instances.command('snapshot')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshot(project):
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print("  Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by Snapshot Script")
+
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Snapshots Created")
+
+    return
 
 @instances.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
-    instances = filter_instanes(project)
+    instances = filter_instances(project)
 
     for i in instances:
         tags = { t['Key']: t['Value'] for t in i.tags or []}
@@ -46,7 +124,7 @@ def list_instances(project):
 def stop_instances(project):
     "Stop EC2 instances"
 
-    instances = filter_instanes(project)
+    instances = filter_instances(project)
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
@@ -60,7 +138,7 @@ def stop_instances(project):
 def start_instances(project):
     "Start EC2 instances"
 
-    instances = filter_instanes(project)
+    instances = filter_instances(project)
 
     for i in instances:
         print("Starting {0}...".format(i.id))
@@ -69,4 +147,4 @@ def start_instances(project):
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
